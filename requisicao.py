dd309cb6970b62e05d345986812dd316d05e4a3b
Maria Isabel <mipt1612@gmail.com>
2024-03-26 15:14:44 -0300

    Formatar JSON

--- requisicao.py
+++ requisicao.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def personagens():
@@ -5,6 +6,7 @@ def personagens():
     api_url = f'https://last-airbender-api.fly.dev/api/v1/characters'
 
     response = requests.get(api_url)
-    return response.json()
 
-print(f'{personagens()}')
+    print(json.dumps(response.json(), indent=4))
+
+personagens()
