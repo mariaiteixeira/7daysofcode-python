3ce553a60a7504293609869e187509ee4e77143b
Maria Isabel <mipt1612@gmail.com>
2024-04-02 14:55:08 -0300

    Adicionar desafio do terceiro dia

--- requisicao.py
+++ requisicao.py
@@ -1,12 +1,23 @@
 import json
 import requests
+from googletrans import Translator
+
 
 def personagens():
+    api_url = f"https://last-airbender-api.fly.dev/api/v1/characters"
 
-    api_url = f'https://last-airbender-api.fly.dev/api/v1/characters'
+    personagem = (requests.get(api_url)).json()
+    print(type(personagem))
 
-    response = requests.get(api_url)
+    translator = Translator()
 
-    print(json.dumps(response.json(), indent=4))
+    for p in personagem:
+        afiliacao = p.get("affiliation", "")
+        p["affiliacao_traduzida"] = translator.translate(afiliacao, dest="pt").text
+        nome = p.get("name", "")
+        p["name_traduzido"] = translator.translate(nome, dest="pt").text
+
+    print(personagem)
+
 
 personagens()
