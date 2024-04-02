1e027e48c7d5474581ef34c2a891ade379113432
Maria Isabel <mipt1612@gmail.com>
2024-04-02 16:46:25 -0300

    Adicionar desafio do quarto dia

--- avatar/views.py
+++ avatar/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+from googletrans import Translator
 
-# Create your views here.
+def personagens(request):
+
+    api_url = f"https://last-airbender-api.fly.dev/api/v1/characters"
+
+    response = requests.get(api_url)
+
+    personagem = response.json()
+
+    translator = Translator()
+
+    for p in personagem:
+        afiliacao = p.get("affiliation", "")
+        p["affiliacao_traduzida"] = translator.translate(afiliacao, dest="pt").text
+        nome = p.get("name", "")
+        p["name_traduzido"] = translator.translate(nome, dest="pt").text
+
+    return render(request, "index.html", {'personagem': personagem})
